5d885e88b7fd76a09535d3c838970d54c452f35d
knightSarai <knight.sarai.dev@gmail.com>
2023-07-30 09:42:49 +0300

    maze solver

---
+++ src/lib/recursion/maze-solver.ts
@@ -0,0 +1,74 @@
+const dirs = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1]
+]
+
+function walk(
+  maze: string[],
+  wall: string,
+  current: Point,
+  end: Point,
+  seen: boolean[][],
+  path: Point[]
+): boolean{
+  if (
+    current.x < 0 || current.x >= maze[0].length || 
+      current.y < 0 || current.y >= maze.length
+  ) {
+    return false;
+  }
+
+  if (maze[current.y][current.x] === wall) {
+    return false;
+  }
+
+  if(current.x === end.x && current.y === end.y) {
+    path.push(current)
+    return true;
+  }
+
+  if(seen[current.y][current.x]) {
+    return false;
+  }
+
+  seen[current.y][current.x] = true
+  path.push(current)
+
+  for (let i = 0; i < dirs.length; i++) {
+    const [x, y] = dirs[i];
+    if (walk(
+      maze,
+      wall,
+      {
+        x: current.x + x,
+        y: current.y +y
+      },
+      end,
+      seen,
+      path
+
+    )) {
+      return true
+    }
+  }
+  path.pop();
+
+  return false
+
+}
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+  const seen: boolean[][]= [];
+  const path: Point[] = [];
+
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false))
+  }
+
+  walk(maze, wall, start, end, seen, path)
+
+  return path
+
+}
+
