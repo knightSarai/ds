c535e5d867cab56db46015cb6ce053ce0ba33ecd
knightSarai <knight.sarai.dev@gmail.com>
2023-09-14 11:50:03 +0300

    Done with tries

--- src/lib/trie/trie.spec.ts
+++ src/lib/trie/trie.spec.ts
@@ -13,4 +13,34 @@ describe('test trie', () => {
         expect(trie.search("apples")).toBeFalsy();
     })
 
+    it('should be get words', () => {
+        const trie = new Trie();
+        trie.insert("apple");
+        trie.insert("app");
+        trie.insert("apps");
+        trie.insert("banana");
+
+        expect(trie.getWrods()).toEqual(["apple", "app", "apps", "banana"]);
+    })
+
+    it('should autocomplete with simialar words', () => {
+        const trie = new Trie();
+        trie.insert("apple");
+        trie.insert("app");
+        trie.insert("apps");
+        trie.insert("banana");
+
+        expect(trie.autoComplete('ban')).toEqual(["ana"]);
+        expect(trie.autoComplete('ap')).toEqual(["ple", "p", "ps"]);
+    })
+
+    it('should autocorrect wrong words', () => {
+        const trie = new Trie();
+        trie.insert("apple");
+        trie.insert("app");
+        trie.insert("apps");
+        trie.insert("banana");
+
+        expect(trie.autoCorrect('appqer')).toEqual("apple");
+    })
 })
--- src/lib/trie/trie.ts
+++ src/lib/trie/trie.ts
@@ -32,6 +32,46 @@ export class Trie {
         }
 
         currentNode.children.set("*", null)
+    }
+
+    getWrods(node=null, word='', words=[]): string[] {
+        const currentNode = node || this.root;
+
+        for (const [key, value] of currentNode.children.entries()) {
+            if (key === "*") {
+                words.push(word);
+                continue;
+            }
+
+            this.getWrods(value, word + key, words);
+        }
+
+        return words
 
     }
+
+    autoComplete(prefix: string): string[] {
+        const currentNode = this.search(prefix);
+        if (!currentNode) {
+            return undefined;
+        }
+
+        return this.getWrods(currentNode);
+    }
+
+    autoCorrect(word: string): string {
+        let currentNode = this.root;
+
+        let found = '';
+        for (const char of word) {
+            if (currentNode.children.has(char)) {
+                found += char;
+                currentNode = currentNode.children.get(char);
+                continue
+            }
+            return found + this.getWrods(currentNode)[0]
+        }
+        return word
+    }
+
 }
